2073c5a57c29ee304c5f4f9c2c75bb79e16e3b01
Dan Fox <iamdanfox@gmail.com>
2015-09-27 13:52:09 +0100

    Gulp webpack

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+
+
+gulp.task("webpack", function(callback) {
+    // run webpack
+    webpack(webpackConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            // output options
+        }));
+        callback();
+    });
+});
