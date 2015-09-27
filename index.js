dc8e8ad7a45654bad210fc2020980bb4ea4d2b32
Dan Fox <iamdanfox@gmail.com>
2015-09-27 13:11:55 +0100

    Boom everything inlined

--- index.js
+++ index.js
@@ -1,32 +1,133 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import App from './containers/App';
-
-
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component, PropTypes } from 'react';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, bindActionCreators } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from './reducers';
+
+
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+const SET_TO_7 = 'SET_TO_7';
+
+function increment() {
+  return {
+    type: INCREMENT_COUNTER
+  };
+}
+
+function decrement() {
+  return {
+    type: DECREMENT_COUNTER
+  };
+}
+
+function incrementIfOdd() {
+  return (dispatch, getState) => {
+    const { counter } = getState();
+
+    if (counter % 2 === 0) {
+      return;
+    }
+
+    dispatch(increment());
+  };
+}
+
+function incrementAsync(delay = 1000) {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(increment());
+    }, delay);
+  };
+}
+
+
+function setTo7() {
+  return {
+    type: SET_TO_7
+  };
+}
+
+
+
+
+
+class Counter extends Component {
+  render() {
+    const { increment, incrementIfOdd, incrementAsync, decrement, setTo7, counter } = this.props;
+    return (
+      <p>
+        Clicked: {counter} times
+        {' '}
+        <button onClick={increment}>+</button>
+        {' '}
+        <button onClick={decrement}>-</button>
+        {' '}
+        <button onClick={incrementIfOdd}>Increment if odd</button>
+        {' '}
+        <button onClick={() => incrementAsync()}>Increment async</button>
+
+        <button onClick={setTo7}>Set to 7</button>
+      </p>
+    );
+  }
+}
+
+Counter.propTypes = {
+  increment: PropTypes.func.isRequired,
+  incrementIfOdd: PropTypes.func.isRequired,
+  incrementAsync: PropTypes.func.isRequired,
+  decrement: PropTypes.func.isRequired,
+  setTo7: PropTypes.func.isRequired,
+  counter: PropTypes.number.isRequired
+};
+
+
+
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ increment, decrement, incrementAsync, incrementIfOdd, setTo7 }, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+
+
+
+
+
+function counter(state = 0, action) {
+  switch (action.type) {
+  case INCREMENT_COUNTER:
+    return state + 1;
+  case DECREMENT_COUNTER:
+    return state - 1;
+  case SET_TO_7:
+    return 7;
+  default:
+    return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  counter
+});
+
+
+
+
+
+
 
 const createStoreWithMiddleware = applyMiddleware(
   thunk
 )(createStore);
 
-function configureStore(initialState) {
-  const store = createStoreWithMiddleware(reducer, initialState);
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('./reducers', () => {
-      const nextReducer = require('./reducers');
-      store.replaceReducer(nextReducer);
-    });
-  }
-
-  return store;
-}
-
-
-const store = configureStore();
+const store = createStoreWithMiddleware(rootReducer);
 
 React.render(
   <Provider store={store}>
